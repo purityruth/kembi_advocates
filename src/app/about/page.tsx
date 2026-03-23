e54db06a5b8e3078d7c00d29746ea238cbc2f205
purityruth <purityruth09@gmail.com>
2026-03-23 15:19:44 +0300

    Footer update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,14 +3,15 @@ import AboutPhilosophy from "@/components/about/AboutPhilosophy";
 import AboutPracticeAreas from "@/components/about/AboutPracticeAreas";
 import AboutTeamPreview from "@/components/about/AboutTeamPreview";
 import AboutCTA from "@/components/about/AboutCTA";
+import TeamSection from "@/components/home/TeamSection";
 
 export default function Page() {
   return (
     <>
       <AboutHero />
       <AboutPhilosophy />
-      <AboutPracticeAreas />
-      <AboutTeamPreview />
+      {/* <AboutPracticeAreas /> */}
+      <TeamSection />
       <AboutCTA />
     </>
   );
